c2364add850e2ddae4ce07e64ed6b4c0b57787ef
SIST <SIST@DESKTOP-JRVQGVR>
2019-08-27 15:55:54 +0900

    sql

--- HealthCare/sql/HealthCare.sql
+++ HealthCare/sql/HealthCare.sql
@@ -1,17 +1,21 @@
+select * from client;
+drop table client;
+
 CREATE SEQUENCE SEQ_member
 START WITH 1000
 INCREMENT BY 1; 
 
-
-
-drop table client;
-
 create table CLIENT(
-    cno number(10),
-    NAME VARCHAR2(20),
-    GENDER number(2),
-    ADDRESS VARCHAR(40),
-    PHONE   VARCHAR(20),
+    cno number(10)
+       CONSTRAINT PK_CLIENT_CNO PRIMARY KEY,
+    NAME VARCHAR2(20)
+       CONSTRAINT NN_CLIENT_NAME NOT NULL,
+    GENDER varchar2(1) 
+       CONSTRAINT CK_CLIENT_GENDER CHECK(GENDER IN('M','F')),
+    ADDRESS VARCHAR(40)
+       CONSTRAINT NN_CLIENT_ADDRESS NOT NULL,
+    PHONE   VARCHAR(20)
+       CONSTRAINT NN_CLIENT_PHONE NOT NULL,
     division number(2),
     created_date date,
     birth_date date,
@@ -20,8 +24,12 @@ create table CLIENT(
     end_date date
 );
 
-insert into client values(seq_member.nextval, 'test', 1, 'test123', '010000000', 1, sysdate, sysdate, '헬스', sysdate,add_months(sysdate, 1));
+-- 회원가입 하기
+insert into client values(seq_member.nextval, 'test', UPPER('f'), 'test123', '010000000', 1, sysdate, sysdate, '헬스', sysdate,add_months(sysdate, 1));
 
 select * from client;
-
 desc client;
+-- 로그인 하기
+select * from client where cno = 1021;
+select count(*) from client where cno=1021;
+commit;
